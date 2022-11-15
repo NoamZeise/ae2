71b0a5be8ef78b67a839d1d25b2c2e390cbb59d4
NoamZeise <noam4134@gmail.com>
2022-11-15 22:34:13 +0000

    fifo

--- schedulers.py
+++ schedulers.py
@@ -6,10 +6,15 @@ from process import ProcessStates
 
 class FCFS(SchedulerDES):
     def scheduler_func(self, cur_event):
-        pass
+        self.processes[cur_event.process_id].process_state = ProcessStates.READY
+        return self.processes[cur_event.process_id]
 
     def dispatcher_func(self, cur_process):
-        pass
+        cur_process.process_state = ProcessStates.RUNNING
+        time = cur_process.run_for(cur_process.remaining_time, self.time)
+        cur_process.process_state = ProcessStates.TERMINATED
+        return Event(process_id=cur_process.process_id, event_type=EventTypes.PROC_CPU_DONE, event_time=time)
+        
 
 
 class SJF(SchedulerDES):
